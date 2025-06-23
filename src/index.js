e2ad6b914c1279dad86302df9e285cfd176d5ff0
prashantdevada <prashantdevada@gmail.com>
2025-06-23 19:50:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // ✅ Add this line
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
