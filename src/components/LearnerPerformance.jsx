b2eb5d9d5c74a28752bb8209b8618bf5c074721c
prashantdevada <prashantdevada@gmail.com>
2025-09-07 16:41:31 +0530

    Update LearnerPerformance.jsx

--- src/components/LearnerPerformance.jsx
+++ src/components/LearnerPerformance.jsx
@@ -144,7 +144,7 @@ const LearnerPerformance = () => {
                   <th>RANK</th>
                   <th>MM</th>
                   <th>OMR</th>
-                  <th>Key</th>
+                  <th>KEY</th>
                 </tr>
               </thead>
               <tbody>
@@ -163,7 +163,7 @@ const LearnerPerformance = () => {
                       <td>{learner['Total']}</td>
                       <td>{learner['%AGE']}</td>
                       <td>{learner['Rank']}</td>
-                      <td>{learner['MM']}</td>
+                      <td>{learner['Max. Marks']}</td>
                       <td>
                         <Button
                           variant="primary"
