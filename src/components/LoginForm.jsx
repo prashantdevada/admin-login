8e867b17ac2f30b50e3c38102e1cdaf203ba6760
prashantdevada <prashantdevada@gmail.com>
2025-09-08 12:30:06 +0530

    API Update2

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -39,7 +39,7 @@ const LoginForm = () => {
       return;
     }
 
-    fetch('https://sheetdb.io/api/v1/lguxn22cung7r?ignore_cache=1')
+    fetch('https://sheetdb.io/api/v1/wjxhjjo0jyqo5?ignore_cache=1')
       .then((response) => response.json())
       .then((data) => {
         const trimmedUsername = username.trim();
