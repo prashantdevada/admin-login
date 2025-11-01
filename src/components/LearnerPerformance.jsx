e3595c5190a294c51cdf85a463d0b6796854bcb1
prashantdevada <prashantdevada@gmail.com>
2025-11-01 23:19:18 +0530

    Update LearnerPerformance.jsx

--- src/components/LearnerPerformance.jsx
+++ src/components/LearnerPerformance.jsx
@@ -192,9 +192,9 @@ const LearnerPerformance = () => {
               alt="Unacademy Logo"
               style={{ height: '60px', maxWidth: '80%' }}
             />
-            <h2 className="mt-2">AHMEDABAD CENTRE</h2>
+            <h2 className="mt-2">PUNE Region- HIPO Learners</h2>
             <p className="centre-address small">
-              Address: 1st and 2nd floor, Zion Z1, Sindhu Bhavan Marg, near Maple County Road, Bodakdev, Ahmedabad, Gujarat 380054
+              
             </p>
           </div>
 
