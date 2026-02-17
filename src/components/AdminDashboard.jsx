00d4f88d72e4005169e618e0ae958a638542c408
prashantdevada <prashantdevada@gmail.com>
2026-02-17 22:50:38 +0530

    Fix formatting of API URL in handleSearch function

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -27,8 +27,7 @@ const AdminDashboard = () => {
     if (!searchRollNo) return;
     setLoading(true);
 
-    const apiURL = `https://script.google.com/macros/s/AKfycbwXvtwuQXtg0YwlEoeADDI58W4vX9JdL2DCu49y5MUnP6e4eSOuA1P8r0HAbzYurA/exec
-?roll=${encodeURIComponent(searchRollNo)}`;
+    const apiURL = `https://script.google.com/macros/s/AKfycbwXvtwuQXtg0YwlEoeADDI58W4vX9JdL2DCu49y5MUnP6e4eSOuA1P8r0HAbzYurA/exec?roll=${encodeURIComponent(searchRollNo)}`;
 
     fetch(apiURL)
       .then((res) => res.json())
